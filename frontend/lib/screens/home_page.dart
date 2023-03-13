124875f6f4e12519393f69933151ff52bc61471f
Bappy60 <kawsar.ahmed.bappy.148@gmail.com>
2023-03-13 21:19:25 +0600

    booking page and doctor details page added

--- frontend/lib/screens/home_page.dart
+++ frontend/lib/screens/home_page.dart
@@ -139,7 +139,9 @@ class _HomePageState extends State<HomePage> {
               Config.spaceSmall,
               Column(
                 children: List.generate(10, (index) {
-                  return const DoctorCard();
+                  return const DoctorCard(
+                    route: 'doc_details',
+                  );
                 }),
               ),
             ],
